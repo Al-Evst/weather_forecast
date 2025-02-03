e2ff3ab25ea0debf02b46bcfa2282e3d30ff1390
Al-Evst <al.evst96@gmail.com>
2025-02-03 22:08:52 +0100

    Update weather1.py

--- weather1.py
+++ weather1.py
@@ -1,11 +1,17 @@
 import requests
 
-parameters = {"MmnqT":"",
+
+def get_weather():
+
+    parameters = {"MmnqT":"",
               "lang":"ru", }
-locations = ['London', 'Cherepovets', 'svo']
-url_template = 'https://wttr.in/{}'
-for location in locations:
-    url = url_template.format(location)
-    response = requests.get(url, params=parameters)
-    response.raise_for_status()
-    print(response.text)
+    locations = ['London', 'Cherepovets', 'svo']
+    url_template = 'https://wttr.in/{}'
+    for location in locations:
+        url = url_template.format(location)
+        response = requests.get(url, params=parameters)
+        response.raise_for_status()
+        print(response.text)  
+            
+if __name__ == '__main__':
+    get_weather()
